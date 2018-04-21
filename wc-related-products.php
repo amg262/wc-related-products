512e6e8cb05fa09c51a28a2b0b4fec004ddb6524
Andrew Gunn <andrewmgunn26@gmail.com>
2018-04-21 17:20:04 -0500

    works. back to beginning jsut leave it

--- wc-related-products.php
+++ wc-related-products.php
@@ -8,7 +8,6 @@ Author:      Scott Nelle
 Author URI:  http://scottnelle.com
 */
 
-namespace WC_RP;
 /**
  * Force related products to show if some have been selected.
  * This is required for WooCommerce 3.0, which will not display products if
@@ -19,13 +18,13 @@ namespace WC_RP;
  *
  * @return bool Modified value - should we force related products to display?
  */
-function crp_force_display( $result, $product_id ) {
+function wc_rp_force_display( $result, $product_id ) {
 	$related_ids = get_post_meta( $product_id, '_related_ids', true );
 
 	return empty( $related_ids ) ? $result : true;
 }
 
-add_filter( 'woocommerce_product_related_posts_force_display', 'crp_force_display', 10, 2 );
+add_filter( 'woocommerce_product_related_posts_force_display', 'wc_rp_force_display', 10, 2 );
 
 /**
  * Determine whether we want to consider taxonomy terms when selecting related products.
@@ -36,22 +35,22 @@ add_filter( 'woocommerce_product_related_posts_force_display', 'crp_force_displa
  *
  * @return bool Modified value - should we consider tax terms during selection?
  */
-function crp_taxonomy_relation( $result, $product_id ) {
+function wc_rp_taxonomy_relation( $result, $product_id ) {
 	$related_ids = get_post_meta( $product_id, '_related_ids', true );
 	if ( ! empty( $related_ids ) ) {
 		return false;
 	} else {
-		return 'none' === get_option( 'crp_empty_behavior' ) ? false : $result;
+		return 'none' === get_option( 'wc_rp_empty_behavior' ) ? false : $result;
 	}
 }
 
-add_filter( 'woocommerce_product_related_posts_relate_by_category', 'crp_taxonomy_relation', 10, 2 );
-add_filter( 'woocommerce_product_related_posts_relate_by_tag', 'crp_taxonomy_relation', 10, 2 );
+add_filter( 'woocommerce_product_related_posts_relate_by_category', 'wc_rp_taxonomy_relation', 10, 2 );
+add_filter( 'woocommerce_product_related_posts_relate_by_tag', 'wc_rp_taxonomy_relation', 10, 2 );
 
 /**
  * Add related products selector to product edit screen
  */
-function crp_select_related_products() {
+function wc_rp_select_related_products() {
 	global $post, $woocommerce;
 	$product_ids = array_filter( array_map( 'absint', (array) get_post_meta( $post->ID, '_related_ids', true ) ) );
 	?>
@@ -116,7 +115,7 @@ function crp_select_related_products() {
 	<?php
 }
 
-add_action( 'woocommerce_product_options_related', 'crp_select_related_products' );
+add_action( 'woocommerce_product_options_related', 'wc_rp_select_related_products' );
 
 /**
  * Save related products selector on product edit screen.
@@ -124,7 +123,7 @@ add_action( 'woocommerce_product_options_related', 'crp_select_related_products'
  * @param int $post_id ID of the post to save.
  * @param obj WP_Post object.
  */
-function crp_save_related_products( $post_id, $post ) {
+function wc_rp_save_related_products( $post_id, $post ) {
 	global $woocommerce;
 	if ( isset( $_POST['related_ids'] ) ) {
 		// From 2.3 until the release before 3.0 Woocommerce posted these as a comma-separated string.
@@ -146,7 +145,7 @@ function crp_save_related_products( $post_id, $post ) {
 	}
 }
 
-add_action( 'woocommerce_process_product_meta', 'crp_save_related_products', 10, 2 );
+add_action( 'woocommerce_process_product_meta', 'wc_rp_save_related_products', 10, 2 );
 
 /**
  * Filter the related product query args.
@@ -156,19 +155,19 @@ add_action( 'woocommerce_process_product_meta', 'crp_save_related_products', 10,
  *
  * @return array Modified query arguments.
  */
-function crp_filter_related_products_legacy( $args ) {
+function wc_rp_filter_related_products_legacy( $args ) {
 	global $post;
 	$related = get_post_meta( $post->ID, '_related_ids', true );
 	if ( $related ) { // remove category based filtering
 		$args['post__in'] = $related;
-	} elseif ( get_option( 'crp_empty_behavior' ) == 'none' ) { // don't show any products
+	} elseif ( get_option( 'wc_rp_empty_behavior' ) == 'none' ) { // don't show any products
 		$args['post__in'] = [ 0 ];
 	}
 
 	return $args;
 }
 
-add_filter( 'woocommerce_related_products_args', 'crp_filter_related_products_legacy' );
+add_filter( 'woocommerce_related_products_args', 'wc_rp_filter_related_products_legacy' );
 
 /**
  * Filter the related product query args.
@@ -178,7 +177,7 @@ add_filter( 'woocommerce_related_products_args', 'crp_filter_related_products_le
  *
  * @return array Modified query arguments.
  */
-function crp_filter_related_products( $query, $product_id ) {
+function wc_rp_filter_related_products( $query, $product_id ) {
 	$related_ids = get_post_meta( $product_id, '_related_ids', true );
 	if ( ! empty( $related_ids ) && is_array( $related_ids ) ) {
 		$related_ids    = implode( ',', array_map( 'absint', $related_ids ) );
@@ -188,30 +187,30 @@ function crp_filter_related_products( $query, $product_id ) {
 	return $query;
 }
 
-add_filter( 'woocommerce_product_related_posts_query', 'crp_filter_related_products', 20, 2 );
+add_filter( 'woocommerce_product_related_posts_query', 'wc_rp_filter_related_products', 20, 2 );
 
 
 /**
  * Create the menu item.
  */
-function crp_create_menu() {
-	add_submenu_page( 'woocommerce', 'Custom Related Products', 'Custom Related Products', 'manage_options', 'custom_related_products', 'crp_settings_page' );
+function wc_rp_create_menu() {
+	add_submenu_page( 'woocommerce', 'Custom Related Products', 'Custom Related Products', 'manage_options', 'custom_related_products', 'wc_rp_settings_page' );
 }
 
-add_action( 'admin_menu', 'crp_create_menu', 99 );
+add_action( 'admin_menu', 'wc_rp_create_menu', 99 );
 
 /**
  * Create the settings page.
  */
-function crp_settings_page() {
+function wc_rp_settings_page() {
 	if ( isset( $_POST['submit_custom_related_products'] ) && current_user_can( 'manage_options' ) ) {
 		check_admin_referer( 'custom_related_products', '_custom_related_products_nonce' );
 
 		// save settings
-		if ( isset( $_POST['crp_empty_behavior'] ) && $_POST['crp_empty_behavior'] != '' ) {
-			update_option( 'crp_empty_behavior', $_POST['crp_empty_behavior'] );
+		if ( isset( $_POST['wc_rp_empty_behavior'] ) && $_POST['wc_rp_empty_behavior'] != '' ) {
+			update_option( 'wc_rp_empty_behavior', $_POST['wc_rp_empty_behavior'] );
 		} else {
-			delete_option( 'crp_empty_behavior' );
+			delete_option( 'wc_rp_empty_behavior' );
 		}
 
 		echo '<div id="message" class="updated"><p>Settings saved</p></div>';
@@ -221,13 +220,13 @@ function crp_settings_page() {
     <div class="wrap" id="custom-related-products">
         <h2>Custom Related Products</h2>
 		<?php
-		$behavior_none_selected = ( get_option( 'crp_empty_behavior' ) == 'none' ) ? 'selected="selected"' : '';
+		$behavior_none_selected = ( get_option( 'wc_rp_empty_behavior' ) === 'none' ) ? 'selected="selected"' : '';
 
 		echo '
 		<form method="post" action="admin.php?page=custom_related_products">
 			' . wp_nonce_field( 'custom_related_products', '_custom_related_products_nonce', true, false ) . '
 			<p>If I have not selected related products:
-				<select name="crp_empty_behavior">
+				<select name="wc_rp_empty_behavior">
 					<option value="">Select random related products by category</option>
 					<option value="none" ' . $behavior_none_selected . '>Don&rsquo;t show any related products</option>
 				</select>
